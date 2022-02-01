5fdab2f280bd92ce86bb46a6f32ac15ce4e4620f
habvi <a2231250601h@yahoo.co.jp>
2022-02-01 16:27:56 +0900

    not open browser

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ import chromedriver_binary
 # 一旦千代田区図書館の開館or閉館情報を調べる
 URL = 'https://www.library.chiyoda.tokyo.jp/'
 
-driver = webdriver.Chrome('chromedriver')
+# 実行時に毎回ブラウザが立ち上がらないようにする
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+
+# 引数にoptionsを渡す
+driver = webdriver.Chrome('chromedriver',
+                          options=options)
 driver.implicitly_wait(10)
 driver.get(URL)
 
-# only chiyoda
 schedule_elm = driver.find_elements_by_xpath(
     '//li[@id="chiyoda-today-status"]/div/div/span')
 
