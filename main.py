1d534ff72e1d44418d98a4c0b504a6eb8aac5b73
habvi <a2231250601h@yahoo.co.jp>
2022-02-01 16:22:54 +0900

    search only chiyoda-ku

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ driver = webdriver.Chrome('chromedriver')
 driver.implicitly_wait(10)
 driver.get(URL)
 
-schedule_elm = driver.find_elements_by_class_name(
-    'schedule-list01__text')
+# only chiyoda
+schedule_elm = driver.find_elements_by_xpath(
+    '//li[@id="chiyoda-today-status"]/div/div/span')
 
 print([s.text for s in schedule_elm])
